1db8c6dce2a62eaa8b61731d7f21bd26583a026d
Sangpisank0398 <mifta0398@outlook.com>
2024-12-17 00:39:32 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -255,7 +255,7 @@ IPVPS=$(curl -s ipinfo.io/ip )
 clear
 figlet 'MIFTAH'
 echo -e "${BIPurple} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "${BIPurple} │                  ${BIWhite}${UWhite}MIFTAH TUNNELING${NC}"
+echo -e "${BIPurple} │                  ${BIWhite}${UWhite}TAJONG TUNNELING${NC}"
 echo -e "${BIPurple} │"
 echo -e "${BIPurple} │  ${BIPurple}OS        :  ${BIYellow}$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' ) ( $( uname -m) )${NC}"
 echo -e "${BIPurple} │  ${BIPurple}CPU       :  ${BIYellow}$cpu_usage${NC}"
